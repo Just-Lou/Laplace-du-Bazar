288c7ed6cabd45afcf009c64b21bc68d3a070ffe
Louis <louisantoine2005@yahoo.com>
2025-05-29 22:21:56 -0400

    1 test fonctionnel et 1 test faux

--- src/test/java/business/AdTest.java
+++ src/test/java/business/AdTest.java
@@ -6,7 +6,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AdTest {
     @Test
-    void testAd() {
+    void ad_priceShouldMatch() {
+        Ad ad = new Ad();
+        ad.setPrice(100.00);
+        assertEquals(100.00, ad.returnPrice());
+    }
+
+    @Test
+    void ad_priceShouldNotMatch() {
         Ad ad = new Ad();
         ad.setPrice(900.00);
         assertEquals(100.00, ad.returnPrice());
