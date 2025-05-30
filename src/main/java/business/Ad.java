f19fd68083ca697198abfaef831f41cc0d648378
C4H9NO2 <amelie2004@hotmail.ca>
2025-05-30 11:31:32 -0400

    classes ad, apt, book & equip

--- src/main/java/business/Ad.java
+++ src/main/java/business/Ad.java
@@ -1,4 +1,37 @@
+package business;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Ad {
-    private String title, description, date, image;
-    private double price;
+    protected UUID adId;
+    protected String title;
+    protected String description;
+    protected LocalDate publicationDate;
+    protected float price;
+    protected int flagQty;
+    protected String folderPath;
+
+    public Ad(String title, String description, LocalDate publicationDate, float price, String folderPath) {
+        this.adId = UUID.randomUUID();
+        this.title = title;
+        this.description = description;
+        this.publicationDate = publicationDate;
+        this.price = price;
+        this.folderPath = folderPath;
+    }
+
+    public UUID getAdId() { return adId; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+    public LocalDate getPublicationDate() { return publicationDate; }
+    public void setPublicationDate(LocalDate publicationDate) { this.publicationDate = publicationDate; }
+    public float getPrice() { return price; }
+    public void setPrice(float price) { this.price = price; }
+    public String getFolderPath() { return folderPath; }
+    public void setFolderPath(String folderPath) { this.folderPath = folderPath; }
+    public int getFlagQty() { return flagQty; }
+    public void setFlagQty(int flagQty) { this.flagQty = flagQty; }
 }
