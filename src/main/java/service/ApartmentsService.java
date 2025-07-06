533967def9c848264d0901e256284527325e9f10
C4H9NO2 <amelie2004@hotmail.ca>
2025-07-06 14:23:40 -0400

    filtre fonctionnels!!!
    *il manque apartmentsize à tester mais l'annonce qu'il y a live a pas de grandeur donc ça fait bug le code

--- src/main/java/service/ApartmentsService.java
+++ src/main/java/service/ApartmentsService.java
@@ -133,15 +133,16 @@ public class ApartmentsService {
     @Path("search")
     @RolesAllowed({"StandardUser", "Administrator", "ExternalUser"})
     public List<ApartmentViewModel> getApartmentsByCriteria(
+            @QueryParam("userId") UUID userId,
             @QueryParam("minPrice") Float minPrice,
             @QueryParam("maxPrice") Float maxPrice,
             @QueryParam("minScore") Float minScore,
-            @QueryParam("disponibilityBefore") String disponibilityBefore,
-            @QueryParam("apartmentSize") String apartmentSize,
+            @QueryParam("disponibilityBefore") LocalDate disponibilityBefore,
+            @QueryParam("apartmentSize") Float apartmentSize,
             @QueryParam("sortBy") String sortBy
     ) {
         return apartmentsMapper.getApartmentsByCriteria(
-                minPrice, maxPrice, minScore, disponibilityBefore, apartmentSize, sortBy
+                userId, minPrice, maxPrice, minScore, disponibilityBefore, apartmentSize, sortBy
         );
     }
 
